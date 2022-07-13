c5c3eabc7c8a632d689bb42d5becff9bb72c9a60
Arun Kumar <arunkumar31786@gmail.com>
2022-07-13 12:43:26 +0530

    security

--- ws/p08-springboot-custom-starter-consumer/src/main/java/com/demo/EnglishConverter.java
+++ ws/p08-springboot-custom-starter-consumer/src/main/java/com/demo/EnglishConverter.java
@@ -1,16 +1,17 @@
 package com.demo;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.demo.api.MessageConverter;
 
 @Component
-//@Primary
-public class JapaneseConverter implements MessageConverter {
+@Primary
+public class EnglishConverter implements MessageConverter {
 
 	@Override
 	public void translate(String msg) {
-		System.out.println("Japanese: "+msg);
+		System.out.println("English: "+msg);
 	}
 	
 }
